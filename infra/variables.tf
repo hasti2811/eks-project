3e8ce23a4c91666774a5d606b4e28d45ba7bef2c
Hasti <hastiamin61@gmail.com>
2026-01-16 21:55:41 +0000

    added default values in root variables.tf for ami, instance type, also added my_ip variable but its value is stored in tfvars

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,20 @@ variable "vpc_cidr" {
   description = "CIDR block for VPC"
   default = "10.0.0.0/16"
 }
+
+variable "my_ip" {
+  type = list(string)
+  description = "my IP address"
+}
+
+variable "ami" {
+  description = "ami"
+  type = string
+  default = "AL2023_x86_64_STANDARD"
+}
+
+variable "instance_type" {
+  description = "instance type"
+  type = list(string)
+  default = ["t3.medium"]
+}
