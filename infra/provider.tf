ca7fb1a25e67e51801649bfdd1a9efcc7186783d
Hasti <hastiamin61@gmail.com>
2026-01-29 15:08:13 +0000

    removed terraform helm provider

--- infra/provider.tf
+++ infra/provider.tf
@@ -36,21 +36,21 @@ provider "aws" {
   }
 }
 
-data "aws_eks_cluster" "example" {
-  name = "example"
-}
+# data "aws_eks_cluster" "example" {
+#   name = "example"
+# }
 
-data "aws_eks_cluster_auth" "example" {
-  name = "example"
-}
+# data "aws_eks_cluster_auth" "example" {
+#   name = "example"
+# }
 
-provider "helm" {
-  kubernetes = {
-    host                   = data.aws_eks_cluster.example.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.example.token
-  }
-}
+# provider "helm" {
+#   kubernetes = {
+#     host                   = data.aws_eks_cluster.example.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.example.token
+#   }
+# }
 
 # provider "kubernetes" {
 #   host                   = data.aws_eks_cluster.example.endpoint
